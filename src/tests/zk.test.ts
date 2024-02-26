9d0c3ce677a172873dd775c6a6fe90436a7ced43
ocn <splines-cameos.0r@icloud.com>
2024-02-26 13:44:49 -0500

    fix(embed): use for minNumInvolved or ship filter
    Signed-off-by: ocn <splines-cameos.0r@icloud.com>

--- src/tests/zk.test.ts
+++ src/tests/zk.test.ts
@@ -1,99 +1,67 @@
 import {Client, Intents} from 'discord.js';
 import {LimitType, Subscription, SubscriptionType, ZkData, ZKillSubscriber} from '../zKillSubscriber';
+import * as fs from 'fs';
+import * as path from 'path';
 
 jest.setTimeout(30000);
+
+const readTestData = (filePath: string): ZkData => {
+    const absolutePath = path.join(__dirname, 'resources', filePath);
+    const fileContent = fs.readFileSync(absolutePath, 'utf-8');
+    return JSON.parse(fileContent);
+};
 
 describe('ZK Subscriber', () => {
     it('should send message to discord', async () => {
         const client = new Client({intents: [Intents.FLAGS.GUILDS]});
         await client.login(process.env.DISCORD_BOT_TOKEN);
         console.log('logged in');
-        const sub = ZKillSubscriber.getInstance(client)
+        const sub = ZKillSubscriber.getInstance(client, false)
             .withSystems('../config/')
             .withShips('../config/')
             .withNames('../config/');
+        const subscription = {
+            'exclusionLimitAlsoComparesAttacker': true,
+            'exclusionLimitAlsoComparesAttackerWeapons': true,
+            'inclusionLimitAlsoComparesAttacker': true,
+            'inclusionLimitAlsoComparesAttackerWeapons': true,
+            'limitTypes': new Map([
+                [LimitType.SECURITY_MIN_INCLUSIVE, '0.0'],
+                [LimitType.SECURITY_MAX_EXCLUSIVE, '0.5'],
+            ]),
+            'minValue': 0,
+            'subType': SubscriptionType.PUBLIC,
+        };
         client.once('ready', async () => {
-            console.log('ready');
-            const zk_data = {
-                'attackers': [
-                    {
-                        'alliance_id': 1411711376,
-                        'character_id': 2117381670,
-                        'corporation_id': 98462540,
-                        'damage_done': 17841,
-                        'final_blow': true,
-                        'security_status': 5,
-                        'ship_type_id': 11377,
-                        'weapon_type_id': 27351
-                    },
-                    {
-                        'alliance_id': 99011181,
-                        'character_id': 2114153944,
-                        'corporation_id': 98678227,
-                        'damage_done': 0,
-                        'final_blow': false,
-                        'security_status': 0.7,
-                        'ship_type_id': 621,
-                        'weapon_type_id': 1877
-                    },
-                    {
-                        'alliance_id': 1411711376,
-                        'character_id': 2118301372,
-                        'corporation_id': 98382886,
-                        'damage_done': 0,
-                        'final_blow': false,
-                        'security_status': 5,
-                        'ship_type_id': 28352,
-                        'weapon_type_id': 8105
-                    },
-                    {
-                        'alliance_id': 99011260,
-                        'character_id': 2113631998,
-                        'corporation_id': 98651532,
-                        'damage_done': 0,
-                        'final_blow': false,
-                        'security_status': 5,
-                        'ship_type_id': 670,
-                        'weapon_type_id': 2420
-                    }
-                ],
-                'killmail_id': 106140056,
-                'killmail_time': '2023-01-17T01:53:02Z',
-                'solar_system_id': 30000594,
-                'victim': {
-                    'alliance_id': 99005338,
-                    'corporation_id': 98636576,
-                    'damage_taken': 17841,
-                    'items': [],
-                    'position': {
-                        'x': -81529320662.88177,
-                        'y': 14731476610.582806,
-                        'z': -180418401167.63428
-                    },
-                    'ship_type_id': 26892
-                },
-                'zkb': {
-                    'locationID': 50002711,
-                    'hash': 'e6ad2b2b40536d05ad96428e24795af21ce9a9d7',
-                    'fittedValue': 3616323.29,
-                    'droppedValue': 0,
-                    'destroyedValue': 3616323.29,
-                    'totalValue': 3616323.29,
-                    'points': 1,
-                    'npc': false,
-                    'solo': false,
-                    'awox': false,
-                    'esi': 'https://esi.evetech.net/latest/killmails/106140056/e6ad2b2b40536d05ad96428e24795af21ce9a9d7/',
-                    'url': 'https://zkillboard.com/kill/106140056/'
-                }
-            };
             sub.subscribe(SubscriptionType.PUBLIC, '888224317991706685', '1115807643748012072', new Map(), true, true, true, true, 1);
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            await sub.sendMessageToDiscord('888224317991706685', '1115807643748012072', sub, zk_data, 'Mobile Small Warp Disruptor II', 26892, 30, 'RED');
-            console.log('done');
+
+            let zk_data = readTestData('115769073_ostingele.json');
+            await sub.sendMessageToDiscord('888224317991706685', '1115807643748012072', subscription, zk_data, {
+                shipName: 'Zirnitra',
+                typeId: 52907,
+                corpId: 98588237,
+                allianceId: 99012162,
+            }, null, 'GREEN');
+
+            zk_data = readTestData('115787551_astrahus.json');
+            await sub.sendMessageToDiscord('888224317991706685', '1115807643748012072', subscription, zk_data, {
+                shipName: 'Astrahus',
+                typeId: 35832,
+                corpId: 1089040789,
+                allianceId: 386292982,
+            }, null, 'RED');
+
+            zk_data = readTestData('115797013_guardian_fight.json');
+            await sub.sendMessageToDiscord('888224317991706685', '1115807643748012072', subscription, zk_data, null, 30, 'GREEN');
+
+            // await sub.sendMessageToDiscord('888224317991706685', '1115807643748012072', subscription, zk_data, {
+            //     shipName: 'Mobile Small Warp Disruptor II',
+            //     typeId: 26892,
+            //     corpId: 98636576,
+            //     allianceId: 99005338,
+            // }, 3, 'RED');
         });
-        await new Promise(resolve => setTimeout(resolve, 7000));
+        await new Promise(resolve => setTimeout(resolve, 20000));
     });
     it('should compare sec status correctly', async () => {
         const client = new Client({intents: [Intents.FLAGS.GUILDS]});
