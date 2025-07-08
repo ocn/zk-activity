c3d5ff003c7f7dca9e40af42449ad83dd7f140d6
ocn <splines-cameos.0r@icloud.com>
2025-07-07 23:07:14 -0400

    fix: cleanup logging
    Signed-off-by: ocn <splines-cameos.0r@icloud.com>

--- killbot-rust/src/processor.rs
+++ killbot-rust/src/processor.rs
@@ -1,23 +1,23 @@
 use crate::config::{AppState, Filter, FilterNode, Subscription, System};
+use crate::discord_bot::{get_ship_group_id, get_system};
 use crate::models::ZkData;
+use futures::future::{BoxFuture, FutureExt};
 use std::ops::RangeInclusive;
 use std::str::FromStr;
 use std::sync::Arc;
 use tracing::{info, warn};
-use crate::discord_bot::{get_system, get_ship_group_id};
-use futures::future::{BoxFuture, FutureExt};
 
 pub async fn process_killmail(app_state: &Arc<AppState>, zk_data: &ZkData) -> Vec<Subscription> {
     let mut matched_subscriptions = Vec::new();
     let subscriptions_map = app_state.subscriptions.read().unwrap();
     let kill_id = zk_data.killmail.killmail_id;
 
-    for subscriptions_vec in subscriptions_map.values() {
+    for (guild_id, subscriptions_vec) in subscriptions_map.iter() {
+        info!(
+            "[Kill: {}] Evaluating kill against '{}' subscriptions for guild ID {}",
+            kill_id, subscriptions_vec.len(), guild_id.0
+        );
         for subscription in subscriptions_vec.iter() {
-            info!(
-                "[Kill: {}] Evaluating subscription '{}' for channel {}",
-                kill_id, subscription.id, subscription.action.channel_id
-            );
             if evaluate_filter_node(&subscription.root_filter, zk_data, app_state).await {
                 matched_subscriptions.push(subscription.clone());
             }
@@ -76,57 +76,72 @@ fn distance(system1: &System, system2: &System) -> f64 {
 
 async fn evaluate_filter(filter: &Filter, zk_data: &ZkData, app_state: &Arc<AppState>) -> bool {
     let killmail = &zk_data.killmail;
-    
+
     match filter {
         Filter::TotalValue { min, max } => {
             let total_value = zk_data.zkb.total_value;
-            min.map_or(true, |m| total_value >= m as f64) && max.map_or(true, |m| total_value <= m as f64)
+            min.map_or(true, |m| total_value >= m as f64)
+                && max.map_or(true, |m| total_value <= m as f64)
         }
         Filter::DroppedValue { min, max } => {
             let dropped_value = zk_data.zkb.dropped_value;
-            min.map_or(true, |m| dropped_value >= m as f64) && max.map_or(true, |m| dropped_value <= m as f64)
+            min.map_or(true, |m| dropped_value >= m as f64)
+                && max.map_or(true, |m| dropped_value <= m as f64)
         }
-        Filter::Region(region_ids) => {
-            get_system(app_state, killmail.solar_system_id).await.map_or(false, |s| region_ids.contains(&s.region_id))
-        }
+        Filter::Region(region_ids) => get_system(app_state, killmail.solar_system_id)
+            .await
+            .map_or(false, |s| region_ids.contains(&s.region_id)),
         Filter::System(system_ids) => system_ids.contains(&killmail.solar_system_id),
         Filter::Security(range_str) => {
-            if let (Some(system), Ok(range)) = (get_system(app_state, killmail.solar_system_id).await, parse_security_range(range_str)) {
+            if let (Some(system), Ok(range)) = (
+                get_system(app_state, killmail.solar_system_id).await,
+                parse_security_range(range_str),
+            ) {
                 range.contains(&system.security_status)
             } else {
                 false
             }
         }
         Filter::Alliance(alliance_ids) => {
-            killmail.victim.alliance_id.map_or(false, |id| alliance_ids.contains(&id))
+            killmail
+                .victim
+                .alliance_id
+                .map_or(false, |id| alliance_ids.contains(&id))
                 || killmail
                     .attackers
                     .iter()
                     .any(|a| a.alliance_id.map_or(false, |id| alliance_ids.contains(&id)))
         }
         Filter::Corporation(corporation_ids) => {
-            killmail.victim.corporation_id.map_or(false, |id| corporation_ids.contains(&id))
-                || killmail
-                    .attackers
-                    .iter()
-                    .any(|a| a.corporation_id.map_or(false, |id| corporation_ids.contains(&id)))
+            killmail
+                .victim
+                .corporation_id
+                .map_or(false, |id| corporation_ids.contains(&id))
+                || killmail.attackers.iter().any(|a| {
+                    a.corporation_id
+                        .map_or(false, |id| corporation_ids.contains(&id))
+                })
         }
         Filter::Character(character_ids) => {
-            killmail.victim.character_id.map_or(false, |id| character_ids.contains(&id))
-                || killmail
-                    .attackers
-                    .iter()
-                    .any(|a| a.character_id.map_or(false, |id| character_ids.contains(&id)))
+            killmail
+                .victim
+                .character_id
+                .map_or(false, |id| character_ids.contains(&id))
+                || killmail.attackers.iter().any(|a| {
+                    a.character_id
+                        .map_or(false, |id| character_ids.contains(&id))
+                })
         }
         Filter::ShipType(ship_type_ids) => {
             ship_type_ids.contains(&killmail.victim.ship_type_id)
-                || killmail
-                    .attackers
-                    .iter()
-                    .any(|a| a.ship_type_id.map_or(false, |id| ship_type_ids.contains(&id)))
+                || killmail.attackers.iter().any(|a| {
+                    a.ship_type_id
+                        .map_or(false, |id| ship_type_ids.contains(&id))
+                })
         }
         Filter::ShipGroup(ship_group_ids) => {
-            if let Some(group_id) = get_ship_group_id(app_state, killmail.victim.ship_type_id).await {
+            if let Some(group_id) = get_ship_group_id(app_state, killmail.victim.ship_type_id).await
+            {
                 if ship_group_ids.contains(&group_id) {
                     return true;
                 }
@@ -150,11 +165,17 @@ async fn evaluate_filter(filter: &Filter, zk_data: &ZkData, app_state: &Arc<AppS
                             return true;
                         }
                     } else {
-                        warn!("Could not find target system {} for LY range check", target_system_id);
+                        warn!(
+                            "Could not find target system {} for LY range check",
+                            target_system_id
+                        );
                     }
                 }
             } else {
-                warn!("Could not find killmail system {} for LY range check", killmail.solar_system_id);
+                warn!(
+                    "Could not find killmail system {} for LY range check",
+                    killmail.solar_system_id
+                );
             }
             false
         }
