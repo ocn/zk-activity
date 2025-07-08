1214f586f9d9ef042bdd9b3aa894f2c783de0503
ocn <splines-cameos.0r@icloud.com>
2025-07-08 17:53:40 -0400

    fix: debug cannot send to channel
    Signed-off-by: ocn <splines-cameos.0r@icloud.com>

--- killbot-rust/src/esi.rs
+++ killbot-rust/src/esi.rs
@@ -123,7 +123,7 @@ impl EsiClient {
             "{}nearestCelestial.php?solarsystemid={}&x={}&y={}&z={}",
             FUZZWORK_URL, system_id, x, y, z
         );
-        tracing::info!("Fetching celestial data from Fuzzwork: {}", url);
+        tracing::trace!("Fetching celestial data from Fuzzwork: {}", url);
 
         let response = self.client.get(&url).send().await?;
         if !response.status().is_success() {
